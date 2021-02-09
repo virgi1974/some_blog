2602eca1722bc4aec5f81ce7a296c44f810e161a
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-09 22:53:28 +0100

    Rendering blogs

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {title: "title by virgilio", body: "Lorem ipsum dolor sit amet consectetur", author: "virgilio", id: 1},
+    {title: "title by Ari", body: "xxxxxxxxxxxxxx", author: "Ariadna", id: 2},
+    {title: "title by Sele", body: "yyyyyyyyyyyyyy", author: "Selene", id: 3}
+  ]);
+
   return ( 
     <div className="home">
-      <h2>Home page</h2>
+      {blogs.map((blog) => {
+        return <div className="blog-preview" key={blog.id}>
+                  <h2>{ blog.title}  </h2>
+                  <p>written by { blog.author }</p>
+                </div>
+      })}
     </div>
  );
 }
