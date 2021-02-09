acac0a811eb1bfc535122950d89bdf9e297fef79
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-09 23:37:39 +0100

    Deleting blog
    - function to delete in parent component goes downward as props

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,4 +1,4 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
   return ( 
     <div className="home">
       <h2>{title}</h2>
@@ -6,6 +6,13 @@ const BlogList = ({ blogs, title }) => {
       return <div className="blog-preview" key={blog.id}>
                 <h2>{ blog.title}  </h2>
                 <p>written by { blog.author }</p>
+                <button onClick={() => handleDelete(blog.id)}
+                        style={{
+                          backgroundColor: "#c9ccf4",
+                          padding: "5px 10px",
+                          marginTop: "6px",
+                          fontSize: "15px"
+                        }}>Delete Blog</button>
               </div>
     })}
   </div>
--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,27 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
-    {title: "title by virgilio", body: "Lorem ipsum dolor sit amet consectetur", author: "virgilio", id: 1},
-    {title: "title by Ari", body: "xxxxxxxxxxxxxx", author: "Ariadna", id: 2},
-    {title: "title by Sele", body: "yyyyyyyyyyyyyy", author: "Selene", id: 3}
+    {title: "title-1 by virgilio", body: "Lorem ipsum dolor sit amet consectetur", author: "virgilio", id: 1},
+    {title: "title-1 by Ari", body: "xxxxxxxxxxxxxx", author: "Ariadna", id: 2},
+    {title: "title-1 by Sele", body: "yyyyyyyyyyyyyy", author: "Selene", id: 3},
+    {title: "title-2 by Ari", body: "zzzzzzzzzzzzzzz", author: "Ariadna", id: 4},
+    {title: "title-2 by Sele", body: "jjjjjjjjjjjjjj", author: "Selene", id: 5}
   ]);
 
+  const handleDelete = (id) => {
+    console.log("deleting log - ", id);
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
   return ( 
-    <BlogList blogs={blogs} title="All the blogs"/>
+    <>
+      <BlogList blogs={blogs} 
+                title="All the blogs"
+                handleDelete={handleDelete}
+      />
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author == "Selene")} title="Selene's blogs"/> */}
+    </>
  );
 }
  
