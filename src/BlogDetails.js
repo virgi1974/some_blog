d78af170460334bac1833918a356909a9b42f949
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-11 23:08:19 +0100

    delete blog action

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,19 @@
 import useFetch from "./useFetch";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import spinner from './spinner.gif';
 
 const BlogDetails = () => {
   const{ id } = useParams();
   const { data: blog, isPending, error } = useFetch('http://localhost:8000/blogs/' + id);
+  const history = useHistory();
 
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, {
+      method: 'DELETE'
+    }).then(() => {
+      history.push('/');
+    })
+  }
 
   return ( 
     <div className="blog-details">
@@ -17,6 +25,7 @@ const BlogDetails = () => {
           <h2>{ blog.title }</h2>
           <p>Written by: { blog.author }</p>
           <div>{ blog.body }</div>
+          <button onClick={handleDelete}>delete</button>
         </article>
       )}
     </div>
