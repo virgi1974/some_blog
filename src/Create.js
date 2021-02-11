f8eb1595d576cacd4d39e804ed5aedf8814d71f2
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-11 22:32:07 +0100

    create blog action

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,55 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Ari');
+  const [isPending, setIsPending] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsPending(true);
+    const blog = { title, body, author };
+    
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('New Blog added');
+      setIsPending(false);
+    })
+  }
+
   return ( 
     <div className="create">
       <h2>Add new Blog</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog title:</label>
+        <input 
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea 
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog author:</label>
+        <select 
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}>
+          <option value="Ari">Ari</option>
+          <option value="Selene">Selene</option>
+          <option value="Virgilio">Virgilio</option>
+          <option value="Aran">Aran</option>
+        </select>
+        { !isPending && <button>Add blog</button>}
+        { isPending && <button>Adding blog</button>}
+      </form>
     </div>
    );
 }
