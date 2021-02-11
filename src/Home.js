e3a12f8d77189f8da7a13eaeef437f21ddb11392
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-11 11:10:30 +0100

    loading spinner forced for a second

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,35 @@
 import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import spinner from './spinner.gif';
+
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setisPending] = useState(true);
 
   useEffect(() => {
     console.log('use effect was run');
 
-    fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        console.log("-- blogs from json file --");
-        console.table(data);
-        setBlogs(data);
-      })
-  }, [])
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          console.log("-- blogs from json file --");
+          console.table(data);
+          setBlogs(data);
+          setisPending(false);
+        })
+    }, 1000)
 
-  const handleDelete = (id) => {
-    console.log("deleting log - ", id);
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  }, [])
 
   return ( 
     <>
+      { isPending && <div><img src={spinner} className="App-spinner" alt="spinner" /> Loading...</div> }
       {blogs && <BlogList blogs={blogs} 
                           title="All the blogs"
-                          handleDelete={handleDelete}
       />}
       {/* <BlogList blogs={blogs.filter((blog) => blog.author == "Selene")} title="Selene's blogs"/> */}
     </>
