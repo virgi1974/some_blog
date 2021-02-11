38f6015383e6ed2bd646f0bebadd7595eeca6559
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-11 22:43:25 +0100

    redirect after blog creation

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('Ari');
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,6 +20,7 @@ const Create = () => {
     }).then(() => {
       console.log('New Blog added');
       setIsPending(false);
+      history.push('/');
     })
   }
 
