734ff643867871ee3691c7ebed238b336c64f1fb
Virgilio Fernandez <virgilio.fernandez@qvantel.com>
2021-02-11 15:53:58 +0100

    basic error handling

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ import spinner from './spinner.gif';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setisPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     console.log('use effect was run');
@@ -13,6 +14,10 @@ const Home = () => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          console.log(res);
+          if (!res.ok) {
+            throw Error('could not fetch data from DB')
+          }
           return res.json();
         })
         .then(data => {
@@ -20,6 +25,12 @@ const Home = () => {
           console.table(data);
           setBlogs(data);
           setisPending(false);
+          setError(null);
+        })
+        .catch(err => {
+          // console.error(err.message);
+          setisPending(false);
+          setError(err.message);
         })
     }, 1000)
 
@@ -27,6 +38,7 @@ const Home = () => {
 
   return ( 
     <>
+      { error && <div style={{color: "red"}}>{error}</div> }
       { isPending && <div><img src={spinner} className="App-spinner" alt="spinner" /> Loading...</div> }
       {blogs && <BlogList blogs={blogs} 
                           title="All the blogs"
